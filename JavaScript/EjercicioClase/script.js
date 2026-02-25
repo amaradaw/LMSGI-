567d7647b414c59a01a97ea0efb54728f1cd781e
amaradaw <aeliala473@g.educaand.es>
2026-02-25 13:42:30 +0100

    modificar el enlace

--- JavaScript/EjercicioClase/script.js
+++ JavaScript/EjercicioClase/script.js
@@ -9,5 +9,11 @@ function agregar() {
     const node = document.createTextNode(entrada.value);
 
     ptarea.appendChild(node);
-    tareas.appendChild(ptarea);                                                                                              
+    tareas.appendChild(ptarea);
+    
+    const enlace = document.createElement("a");
+    const node2 = document.createTextNode("Enlace a..");
+    enlace.appendChild(node2);
+    enlace.setAttribute("href", "https://www.google.com");
+    ptarea.appendChild(enlace);
 }
