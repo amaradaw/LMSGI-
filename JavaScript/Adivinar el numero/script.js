46ed5b10ec1721e99e98f88ff4e1ad9165b4ecf8
amaradaw <aeliala473@g.educaand.es>
2026-02-27 11:12:10 +0100

    añadir numero de intentos

--- JavaScript/Adivinar el numero/script.js
+++ JavaScript/Adivinar el numero/script.js
@@ -1,17 +1,23 @@
  const numeroSecreto = Math.floor(Math.random() * 10) + 1;
+let intentos = 0;
 
         document.getElementById("formulario").addEventListener("submit", function(event) {
             event.preventDefault();
 
             const numeroUsuario = parseInt(document.getElementById("numero").value);
             const resultado = document.getElementById("resultado");
+            const intentoS = document.getElementById("intentos");
 
             if (numeroUsuario === numeroSecreto) {
                 resultado.textContent = "Lo has acertado!";
             } else if (numeroUsuario < numeroSecreto) {
                 resultado.textContent = "No es correcto. El número secreto es mayor.";
+                intentos++;
+                intentoS.textContent = "LLevas " + intentos + " intentos.";
             } else {
                 resultado.textContent = "No es correcto. El número secreto es menor.";
+                intentos++;
+                intentoS.textContent = "LLevas " + intentos + " intentos.";
             }
         }
     );
